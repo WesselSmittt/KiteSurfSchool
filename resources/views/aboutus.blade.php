f7aed58e20d5bc03bdefeb35c4b91e018b68e339
Woenzelaar <112466769+Woenzelaar@users.noreply.github.com>
2024-03-18 15:14:42 +0100

    Commit

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+
+
+<div class="bg-cover bg-center h-96 w-full" style="background-image: url('img/Aboutus-header.png');">
+<nav class="p-6 flex justify-between items-center">
+        <div>
+            <img src="../../public/img/logo.png" alt="Logo" class="h-8">
+        </div>
+        <div class="text-2xl font-bold text-gray-800">
+            KITESURF
+        </div>
+        <div>
+            <button class="lg:hidden">
+                <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </button>
+        </div>
+    </nav>
+<h2 class="text-4xl font-bold text-white text-center pt-20">About Us</h2>
+</div>
+</body>
+</body>
+</html>
