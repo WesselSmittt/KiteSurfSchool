07005c53f984cebba9eec5e3bcc7a26eb1a833a7
Woenzelaar <112466769+Woenzelaar@users.noreply.github.com>
2024-03-18 15:34:11 +0100

    Add logo and navbar text to about us page

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -11,23 +11,23 @@
 
 
 <div class="bg-cover bg-center h-96 w-full" style="background-image: url('img/Aboutus-header.png');">
-<nav class="p-6 flex justify-between items-center">
+<div x-data="{ open: false }">
+    <nav class="p-6 flex justify-between items-center">
         <div>
-            <img src="../../public/img/logo.png" alt="Logo" class="h-8">
-        </div>
-        <div class="text-2xl font-bold text-gray-800">
-            KITESURF
+            <img src="{{url('/img/logo.png')}}" alt="Logo" class="h-14">
         </div>
         <div>
-            <button class="lg:hidden">
-                <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-            </button>
+            <img src="{{url('/img/Kitesurftext-navbar.png')}}" alt="Logo" class="h-14">
         </div>
-    </nav>
-<h2 class="text-4xl font-bold text-white text-center pt-20">About Us</h2>
+        <div>
+            <h3 class="text-white font-bold">MENU</h3>
+        </div>
+        
+    
+        
 </div>
+
+
 </body>
 </body>
 </html>
